28a7283d38af6153009f6ac3c7548fc82400fd02
AlexeyPortnovSystemAnalyst <savinopoint@ya.ru>
2024-03-24 18:19:04 +0100

    template init commit

---
+++ microservices/delivery/DeliveryApp.Ui/Startup.cs
@@ -0,0 +1,44 @@
+namespace DeliveryApp.Ui
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables();
+            var configuration = builder.Build();
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Конфигурация
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configuration
+            services.Configure<Settings>(options => Configuration.Bind(options));
+            var connectionString = Configuration["CONNECTION_STRING"];
+            var rabbitMqHost = Configuration["RABBIT_MQ_HOST"];
+
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHealthChecks("/health");
+            app.UseRouting();
+        }
+    }
+}
---
+++ microservices/delivery/Utils/Primitives/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Primitives;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default);
+
+    Task<IDbContextTransaction> BeginTransactionAsync();
+
+    Task CommitTransactionAsync(IDbContextTransaction transaction);
+}
